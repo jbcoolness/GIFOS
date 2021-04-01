936547c0d474ff5cb321f03f38952fb03e9d05cb
jbcoolness <julio.bertty@gmail.com>
2021-03-31 19:48:37 -0500

    added header with buger menu responsive

--- js/script.js
+++ js/script.js
@@ -4,9 +4,22 @@
 // $color-bg-header= rgba(255,255,255,0.50);
 // $color-white= #FFFFFF 100%;
 
+// Efecto de boton menu hamburguesa
+const iconBurger = document.getElementById('iconBurger');
+const menu = document.getElementById('menu');
 
-let btnCreate = document.getElementById("btn-create");
+// Arrow function que me cambia el icon Burger 
+// de pendiendo si está abier o cerrado
+const iconBurgerToggle= () =>{
+    iconBurger.classList.toggle('burger-close');
+}
 
+iconBurger.addEventListener('click', function() {
+    iconBurgerToggle();
+})
+
+// Efecto para boton crear del menu
+const btnCreate = document.getElementById("btn-create");
 btnCreate.addEventListener('mouseover', function () {
     btnCreate.src= "./assets/img/CTA-crear-gifo-hover.svg";
 })
@@ -14,3 +27,24 @@ btnCreate.addEventListener('mouseover', function () {
 btnCreate.addEventListener('mouseout', function () {
     btnCreate.src= "./assets/img/button-crear-gifo.svg";
 })
+
+// Efecto de desplazamiento del menú
+const items = document.getElementsByClassName('items')[0];
+const menuBurger = document.getElementById('iconBurger');
+
+const toggleMenu = ()=> {
+    items.classList.toggle('items2')
+    items.style.transition = "transform 0.3s ease-in-out";
+}
+
+menuBurger.addEventListener('click', function() {
+    toggleMenu();    
+});
+
+window.addEventListener('resize', function(){
+    if (screen.width > 769) {
+        toggleMenu();
+        items.style.transition = 'none';
+    }
+})
+
