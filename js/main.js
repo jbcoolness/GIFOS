f26b23b77c5445d582da73b6018e8ae0f0e4566d
jbcoolness <julio.bertty@gmail.com>
2021-04-15 00:31:12 -0500

    added feature search gifs

--- js/main.js
+++ js/main.js
@@ -1,25 +1,63 @@
-//let apiKey = 'api_key=yFy6hNfjHa8UkN2lAHcIVF1PmSdcvQTC';
+// IMPLEMENTACION EN EL CONSUMO DE LA API
+// Gif tendencias en el slide de tres 
 
-const urlGifsTrneding = 'api.giphy.com/v1/gifs/trending';
-const fetchGifsTrneding = fetch(`http://${urlGifsTrneding}?${apiKey}`).then(response => response.json());
+// async function getTrendingGif(limit_) {
+//     try {
+//         const urlGifsTrneding = 'api.giphy.com/v1/gifs/trending';
+//         //let limit = limit_;
+//         let fetchTrendingGifs = await fetch(`http://${urlGifsTrneding}?${apiKey}&limit=${limit_}`);
+//         let trendingGifs = await fetchTrendingGifs.json();
+//         console.log(trendingGifs.data);
+//         return trendingGifs.data;
+
+//     } catch (error) {
+//         console.log(error);
+//     }
+// };
+
+async function getApi(url) {
+    try {        
+        let fetchTrendingGifs = await fetch(`http://${url}`);
+        let trendingGifs = await fetchTrendingGifs.json();
+        console.log(trendingGifs.data);
+        return trendingGifs.data;
+
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+let urlGifsTrneding = (`api.giphy.com/v1/gifs/trending?api_key=yFy6hNfjHa8UkN2lAHcIVF1PmSdcvQTC&limit=30`)
+
+// variable que recibe los 30 gifs que llegan de la API
+let trendingGifs = getApi(urlGifsTrneding);
+
+// div donde se crearan las cards con los gifs
+const cardsContainer = document.getElementById('cardsContainer')
 const imgCard1 = document.getElementById('imgCard1');
 const imgCard2 = document.getElementById('imgCard2');
 const imgCard3 = document.getElementById('imgCard3');
+
+// flecha derecha e izquierda con el que pasaran las cards con los gifs
 const rightRow = document.getElementById('imgRightRow');
 const leftRow = document.getElementById('imgLeftRow');
-let indexJson = 0;
 
-console.log(fetchGifsTrneding)
-fetchGifsTrneding.then(json => {
-    console.log(indexJson);
-    console.log(json.data.length);
-    imgCard1.setAttribute('src', json.data[indexJson].images.downsized_medium.url);
-    imgCard2.setAttribute('src', json.data[indexJson+1].images.downsized_medium.url);
-    imgCard3.setAttribute('src', json.data[indexJson+2].images.downsized_medium.url);
-    console.log(json.data[0].images.original.mp4);
+// Funcion recibe la variable con gifs y muestra los 3 gifs en el slide
+function printCardsGif(trendingGifs, indexGif) {
+    trendingGifs.then(gifs => {
+        console.log(indexGif);
+        console.log(gifs.length);
+        imgCard1.setAttribute('src', gifs[indexGif].images.downsized_medium.url);
+        imgCard2.setAttribute('src', gifs[indexGif+1].images.downsized_medium.url);
+        imgCard3.setAttribute('src', gifs[indexGif+2].images.downsized_medium.url);
+    })
+};
 
-}).catch(error => {
-    console.log(error);
+// Variable para iterar e ir cambiando los gif del slide
+let indexGif;
+window.addEventListener('load', function () {
+    indexGif = 0;
+    printCardsGif(trendingGifs, indexGif)
 });
 
 // Efecto mouse hover y mouse out sobre flechas de trending
@@ -37,41 +75,73 @@ leftRow.addEventListener('mouseout', function () {
     leftRow.setAttribute('src', "/assets/img/Button-Slider-left.svg")
 });
 
-// en esta funcion vamos corriendo las imagenes para ir avanzando
-// y mostrando las 50 que trae el json
-rightRow.addEventListener('click', function() {
-    if ((indexJson + 2) < 50){
-        indexJson = indexJson+1;
-        fetchGifsTrneding.then(json => {
-            //let indexJson = 0;
-            console.log(indexJson);
-            console.log(json.data.length);
-            imgCard1.setAttribute('src', json.data[indexJson].images.downsized_medium.url);
-            imgCard2.setAttribute('src', json.data[indexJson+1].images.downsized_medium.url);
-            imgCard3.setAttribute('src', json.data[indexJson+2].images.downsized_medium.url);
-            //console.log(json.data[0].images.original.mp4);
-        
-        })
-        
-    }
+// mover las cards
+rightRow.addEventListener('click', function () {
+    if (indexGif < 30) {
+        indexGif = indexGif + 1;
+        printCardsGif(trendingGifs, indexGif);
+        console.log(indexGif);
+    }else {
+        console.log('Limite gif trending');
+    }    
 });
 
-// Esta es la flecha de la izquierda hay que arreglarlo (esta en proceso)
-leftRow.addEventListener('click', function() {
-    if ((indexJson + 2) > 2){
-        indexJson = indexJson-1;
-        fetchGifsTrneding.then(json => {
-            //let indexJson = 0;
-            console.log(indexJson);
-            console.log(json.data.length);
-            imgCard1.setAttribute('src', json.data[indexJson].images.downsized_medium.url);
-            imgCard2.setAttribute('src', json.data[indexJson-1].images.downsized_medium.url);
-            imgCard3.setAttribute('src', json.data[indexJson-2].images.downsized_medium.url);
-            //console.log(json.data[0].images.original.mp4);
-        
-        })
-        
+leftRow.addEventListener('click', function () {
+    if (indexGif > 0) {
+        indexGif = indexGif - 1;
+        printCardsGif(trendingGifs, indexGif);
+        console.log(indexGif);
     }else {
-        console.log('no hay mas gifs')
-    }
+        console.log('Limite gif trending');
+    }    
 });
+
+// Busqueda de gifs mediante Barra
+const searchResult = document.getElementById('searchResult');
+let imgShowMore;
+
+searchBar.addEventListener('focus', function () {
+    searchBar.addEventListener('keypress', function (event) {
+        if (event.key === 'Enter') {
+            let word = searchBar.value;
+            console.log(word);
+            let offset = 12;
+            let urlSearchGifs = (`api.giphy.com/v1/gifs/search?api_key=yFy6hNfjHa8UkN2lAHcIVF1PmSdcvQTC&q=${word}&limit=12&offset=${offset}`);
+            
+            // Variable que almacena el resultado de la API con la consulta
+            let searchGifs = getApi(urlSearchGifs);
+
+            // Recibe la respuesta con 12 gif y los muestra en el div
+            searchGifs.then( gifs => {
+                gifs.forEach( gif => {
+                    let divResult = document.createElement('div');
+                    let imgResult =  document.createElement('img');
+                    divResult.classList.add('div-result');
+                    imgResult.classList.add('img-Result');
+                    searchResult.appendChild(divResult);
+                    divResult.appendChild(imgResult);
+                    imgResult.setAttribute('src', gif.images.downsized_medium.url);
+                    console.log(gif.images.downsized_medium.url)
+                });
+            });
+            
+        }
+    });
+});
+
+// // Variable que almacena el resultado de la API con la consulta
+// let searchGifs = getApi(urlSearchGifs);
+
+// // Recibe la respuesta con 12 gif y los muestra en el div
+// searchGifs.then( gifs => {
+//     gifs.forEach( gif => {
+//         let divResult = document.createElement('div');
+//         let imgResult =  document.createElement('img');
+//         divResult.classList.add('div-result');
+//         imgResult.classList.add('img-Result');
+//         searchResult.appendChild(divResult);
+//         divResult.appendChild(imgResult);
+//         imgResult.setAttribute('src', gif.images.downsized_medium.url);
+//         console.log(gif.images.downsized_medium.url)
+//     });
+// });
